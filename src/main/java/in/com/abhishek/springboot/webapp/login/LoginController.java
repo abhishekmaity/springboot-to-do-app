3a378be5bde85b1d06a196eb2b39f486296ee14c
Abhishek Maity <abhishek.maity@hotmail.com>
2024-01-22 08:49:15 +0530

    logger added

--- src/main/java/in/com/abhishek/springboot/webapp/login/LoginController.java
+++ src/main/java/in/com/abhishek/springboot/webapp/login/LoginController.java
@@ -1,13 +1,20 @@
 package in.com.abhishek.springboot.webapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	@RequestMapping("login")
-	public String gotoLogin() {
+	public String gotoLogin(@RequestParam String name, ModelMap model) {
+		logger.info("Request param is {} ",name);
+		model.put("name", name);
 		return "login";
 	}
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,6 @@
         <title>Login</title>
     </head>
     <body>
-        Welcome to the Login landing page
+        Welcome to the Login landing page ${name} !
     </body>
 </html>
