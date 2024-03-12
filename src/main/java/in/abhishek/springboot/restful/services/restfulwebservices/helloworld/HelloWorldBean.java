313fd5d211b40e2053bd8269163768f3e1153786
Abhishek Maity <abhishek.maity@hotmail.com>
2024-03-12 22:32:33 +0530

    @PathVariable

---
+++ src/main/java/in/abhishek/springboot/restful/services/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package in.abhishek.springboot.restful.services.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+			}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+	
+
+}
---
+++ src/test/java/in/abhishek/springboot/restful/services/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package in.abhishek.springboot.restful.services.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
