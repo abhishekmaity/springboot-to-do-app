7a0d0d012ccf713cefca199ef41e706b2b6cf117
Abhishek Maity <abhishek.maity@hotmail.com>
2024-03-18 00:34:24 +0530

    RequestParams, custom and mediaType versioning

--- src/main/java/in/abhishek/springboot/restful/services/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/in/abhishek/springboot/restful/services/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,34 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionOfPerson() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	@GetMapping(path="/person", params="version=1")
+	public PersonV1 getFirstVersionOfPersonReqParams() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person", params="version=2")
+	public PersonV2 getSecondVersionOfPersonreqParams() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	@GetMapping(path="/person/header", headers ="API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonHeaderParams() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person/header", headers ="API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonHeaderParams() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	@GetMapping(path="/person/accept", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonAccept() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person/accept", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonAccept() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
 }
