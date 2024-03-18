96faeb8adaa366296253c7483bff2e2134b1c088
Abhishek Maity <abhishek.maity@hotmail.com>
2024-03-18 22:51:23 +0530

    post entity manyToOne user entity

--- src/main/java/in/abhishek/springboot/restful/services/restfulwebservices/user/User.java
+++ src/main/java/in/abhishek/springboot/restful/services/restfulwebservices/user/User.java
@@ -1,12 +1,16 @@
 package in.abhishek.springboot.restful.services.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import in.abhishek.springboot.restful.services.restfulwebservices.Post;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -29,6 +33,10 @@ public class User {
 	@JsonProperty("birth_date")
 	@Past(message="Birth Date should be in the past")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
